e1e835096d05697b1272504bd810184460a33846
Nugget-Meister <tremainecamacho@gmail.com>
2023-05-31 18:53:23 -0400

    tests 1-6

--- index.js
+++ index.js
@@ -9,35 +9,56 @@ const newMember = require("./data/new-member");
  * @param {Object} obj -
  * @return {number} The number of keys in the object
  */
-function numberOfKeys(obj) {}
+function numberOfKeys(obj) {
+  return _.keys(obj).length
+}
 
 /**
  * Remove the falsy values in a numbers array and return the sum
  * @param {numbers[]} array - An array of numbers that can also contain some falsy values
  * @return {number} The sum of the numbers in an array
  */
-function sumNumbers(array) {}
+function sumNumbers(array) {
+  return _.reduce(array, function(sum, num){
+    return Number(sum) + Number(num)
+  })
+}
 
 /**
  * Convert a two-dimensional array of new member data (each inner array having two values: the first being the key, the second being the value) into an object
  * @param {Array[]} member -
  * @return {number} The sum of the numbers in an array
  */
-function newMemberArrayToObject(member) {}
+function newMemberArrayToObject(member) {
+  return _.fromPairs(member)
+}
 
 /**
  * Return an array of objects that grouped by instructors from the classes array of objects
  * @param {Object[]} collection - an array of yoga class objects
  * @return {Object[]} - the reshaped collection where the classes are grouped by instructor name
  */
-function groupClassByInstructor(collection) {}
+function groupClassByInstructor(collection) {
+  console.log(collection)
+  let newObj = {}
+  _.each(collection, x => {
+    if(!newObj[x.instructor]){
+      newObj[x.instructor] = [x]
+    } else {
+      newObj[x.instructor].push(x)
+    }
+  })
+  return newObj
+}
 
 /**
  * Remove the age key from the members array of object
  * @param {Object} collection - an array of member objects
  * @return {number} The array of member objects, each one without the age field
  */
-function omitAgeFromMembers(collection) {}
+function omitAgeFromMembers(collection) {
+  return _.omit(collection, "age")
+}
 
 /**
  * Return the count of the number of classes a particular instructor teaches
@@ -45,7 +66,11 @@ function omitAgeFromMembers(collection) {}
  * @param {String} instructorName - The name of the instructor
  * @return {number} The sum of the numbers in an array
  */
-function countClassesByInstructor(collection, instructor) {}
+function countClassesByInstructor(collection, instructor) {
+  let count = 0
+  _.each(collection, (x) => {x.instructor == instructor ? count++ : null})
+  return count != 0 ? count : `There is no instructor by that name.`
+}
 
 /**
  * Remove inactive members from the members array
