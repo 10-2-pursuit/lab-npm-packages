abdda5e92a90fdcd39247833ec4d5c2396478adf
da314jones <dwaynejones@pursuit.org>
2023-06-04 07:31:50 -0400

    Anile C. & Dwayne J. collaberative npm packages

--- index.js
+++ index.js
@@ -9,35 +9,47 @@ const newMember = require("./data/new-member");
  * @param {Object} obj -
  * @return {number} The number of keys in the object
  */
-function numberOfKeys(obj) {}
+function numberOfKeys(obj) {
+  return _.keys(obj).length
 
+};
 /**
  * Remove the falsy values in a numbers array and return the sum
  * @param {numbers[]} array - An array of numbers that can also contain some falsy values
  * @return {number} The sum of the numbers in an array
  */
-function sumNumbers(array) {}
+function sumNumbers(array) {
+  const numbers = _.filter(array, _.isNumber);
+  return _.sum(numbers) 
+    } 
 
 /**
  * Convert a two-dimensional array of new member data (each inner array having two values: the first being the key, the second being the value) into an object
  * @param {Array[]} member -
- * @return {number} The sum of the numbers in an array
+ * @return {Object} The member info as a new object
  */
-function newMemberArrayToObject(member) {}
+function newMemberArrayToObject(member) {
+  return _.fromPairs(member)
+}
 
 /**
  * Return an array of objects that grouped by instructors from the classes array of objects
  * @param {Object[]} collection - an array of yoga class objects
  * @return {Object[]} - the reshaped collection where the classes are grouped by instructor name
  */
-function groupClassByInstructor(collection) {}
+function groupClassByInstructor(collection) {
+  return _.groupBy(collection, "instructor");
+}
+
 
 /**
  * Remove the age key from the members array of object
  * @param {Object} collection - an array of member objects
  * @return {number} The array of member objects, each one without the age field
  */
-function omitAgeFromMembers(collection) {}
+function omitAgeFromMembers(collection) {
+  return _.map(collection, _.partialRight(_.omit, "age"));
+}
 
 /**
  * Return the count of the number of classes a particular instructor teaches
@@ -45,21 +57,33 @@ function omitAgeFromMembers(collection) {}
  * @param {String} instructorName - The name of the instructor
  * @return {number} The sum of the numbers in an array
  */
-function countClassesByInstructor(collection, instructor) {}
+function countClassesByInstructor(collection, instructorName) {
+  const classesByInstructor = _.filter(collection, { instructor: instructorName });
+  const count = _.size(classesByInstructor);
+  return count > 0 ? count : "There is no instructor by that name.";
+}
 
 /**
  * Remove inactive members from the members array
  * @param {Object} collection - an array of member objects
  * @return {number} The array of member objects with only active members
  */
-function removeInactiveMembers(collection) {}
+function removeInactiveMembers(collection,) {
+  return _.filter(collection, { currentMember: true });
+
+}
 
 /**
  * Get a list of unique class titles and their price
  * @param {Object} collection - an array of yoga class objects
  * @return {number} An array of objects that have a unique title and a price
  */
-function getUniqueClasses(collection) {}
+function getUniqueClasses(collection) {
+  // return _.map(collection, obj => obj.title, obj.priceInCents);
+  // return _.uniq(collection, [{title: "title", priceInCents: "priceInCents"}])
+}
+console.log(collections["title"])
+
 
 /**
  * Get a list of classes organized by title, then by level.
